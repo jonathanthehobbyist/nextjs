59036e7e62b921483ea46af4cf1476a9794db5a9
jonathanpsimmons <jon@jonsimmons.co>
2025-02-12 09:00:43 -0600

    committing before I start the day

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,8 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
 
+
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
@@ -14,6 +16,27 @@ const FormSchema = z.object({
 	status: z.enum(['pending','paid']),
 	date: z.string(),
 });
+
+const UpdateInvoice = FormSchema.omit({ id:true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+	const { customerId, amount, status } = UpdateInvoice.parse({
+		customerId: formData.get('customerId'),
+		amount: formData.get('amount'),
+		status: formData.get('status'),
+	});
+
+	const amountInCents = amount * 100;
+
+	await sql`
+		UPDATE invoices
+		SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+		WHERE id = ${id}
+	`;
+
+	revalidatePath('/dashboard/invoices');
+	redirect('/dashboard/invoices');
+};
 
 const CreateInvoice = FormSchema.omit({id: true, date: true});
 
