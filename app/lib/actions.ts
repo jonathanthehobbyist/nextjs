83bcd27cdfbec4cb5dd28e8b452cd014f310ce48
jonathanpsimmons <jon@jonsimmons.co>
2025-02-12 21:13:19 -0600

    working with next-auth for authenticaion

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,14 +25,27 @@ const FormSchema = z.object({
 
 const UpdateInvoice = FormSchema.omit({ id:true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-	const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+	prevState: State,
+	id: string,
+	formData: FormData
+	) {
+	const validatedFields = UpdateInvoice.safeParse({
 		customerId: formData.get('customerId'),
 		amount: formData.get('amount'),
 		status: formData.get('status'),
 	});
 
+	if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+
+  	const { customerId, amount, status } = validatedFields.data;
 	const amountInCents = amount * 100;
+
 try {
 	await sql`
 		UPDATE invoices
@@ -40,7 +53,11 @@ try {
 		WHERE id = ${id}
 	`;
 } catch (error) {
-	console.log(error);
+
+	return {
+		message: 'Database Error: Failed to Update Invoice.',
+	};
+
 }
 
 	revalidatePath('/dashboard/invoices');
